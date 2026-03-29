254a67148f212a3598657dd97143d79478677fb9
ibrahko <wecodefrom@gmail.com>
2026-03-29 22:14:27 +0200

    fix missing render import in backoffice views

--- apps/backoffice/views.py
+++ apps/backoffice/views.py
@@ -4,7 +4,7 @@ from django.views.generic import (
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
 from django.utils import timezone
 
